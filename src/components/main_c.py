8a1d6e5ec787e48ae864f003ab01dc1651d2d158
Kabeeraa27 <kabeeer27@gmail.com>
2024-06-14 04:17:20 +0530

    Fifth Commit

--- src/components/main_c.py
+++ src/components/main_c.py
@@ -1,13 +1,26 @@
-#main_c.py
 import pandas as pd
 from data_ingestion import load_data
 from classification_trainer import train_classification_model
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from src.logger import log_info, log_error
 import os
-import joblib
+import warnings
+import sys
+
+warnings.filterwarnings('ignore', category=UserWarning, module='joblib')
+
 
 def main():
+
+    warnings.filterwarnings("ignore", message="Precision is ill-defined and being set to 0.0 in labels with no predicted samples.")
+    warnings.filterwarnings("ignore", message="Could not find the number of physical cores*", category=UserWarning)
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", message=".*loky.backend.context.*")
+    warnings.filterwarnings("ignore", category=UserWarning, module='joblib')
+    sys.stderr = open(os.devnull, 'w')
+
+
     # Load data
     data_file_path = "C:\\Users\\kabee\\OneDrive\\Desktop\\DS_PROJECT\\notebook\\data\\Obesity Estimation Cleaned.csv"
     data = load_data(data_file_path)
@@ -24,8 +37,11 @@ def main():
         return
 
     # Drop the target column
-    X = data.drop(columns=[target_column])
+    X = data.drop(columns=[target_column, 'BMI'])
     y = data[target_column]
+
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)  # Correct usage on y directly
 
     # Split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
